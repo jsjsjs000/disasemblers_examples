49b94e9a21cb1a7a679d0e00e359174695a227f8
jsjsjs000 <jsjsjs000@gmail.com>
2025-04-07 01:18:38 +0200

    dev

---
+++ README.txt
@@ -0,0 +1,8 @@
+  x64 Ubuntu 22.04
+/lib/x86_64-linux-gnu/libc.so.6
+  lib/
+
+  i.MX Phytec libraries
+/home/jarsulk/phyLinux/build/tmp/work/phyboard_pollux_imx8mp_3-phytec-linux/pco-ml-image/1.0-r0/rootfs/
+  lib/
+  usr/lib/
---
+++ hello_world_c_arm64/env.sh
@@ -0,0 +1,2 @@
+source /opt/ampliphy-vendor-xwayland/BSP-Yocto-NXP-i.MX8MP-PD23.1.0/environment-setup-cortexa53-crypto-phytec-linux
+set sysroot /opt/ampliphy-vendor-xwayland/BSP-Yocto-NXP-i.MX8MP-PD23.1.0/sysroots/cortexa53-crypto-phytec-linux
